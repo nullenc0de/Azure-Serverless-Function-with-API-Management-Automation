40ffa8b6fc7cc491803ba1d8e974af0d384cd536
nullenc0de <pseekamp@gmail.com>
2024-07-12 12:09:22 -0400

    Add files via upload

---
+++ functions/functionapp/function_code.js
@@ -0,0 +1,15 @@
+// functions/functionapp/function_code.js
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
